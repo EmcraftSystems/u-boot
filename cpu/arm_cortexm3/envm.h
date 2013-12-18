8c955863d44cb379ff76d5ae12d33a8503b4592d
Vladimir Khusainov <vlad@emcraft.com>
2013-12-18 19:12:39 +0400

    RT93137 Fix the build issue because of the added function in envm.h. This function only makes sense if CONFIG_ENVM is on (not the case for the STM-SOM configuration).

--- cpu/arm_cortexm3/envm.h
+++ cpu/arm_cortexm3/envm.h
@@ -31,6 +31,8 @@ void envm_init(void);
 # error "LPC43XX ENVM requires RAMCODE"
 #endif
 
+#if defined(CONFIG_ENVM)
+
 /*
  * Check if an address is in the eNVM.
  */
@@ -39,6 +41,8 @@ static inline int envm_addr(unsigned long addr)
 	return addr >= CONFIG_SYS_ENVM_BASE &&
 		addr < CONFIG_SYS_ENVM_BASE + CONFIG_SYS_ENVM_LEN;
 }
+
+#endif
 
 /*
  * Write a data buffer to eNVM.
