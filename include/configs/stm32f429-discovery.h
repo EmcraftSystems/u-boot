8c55cb3e45c74442f6a937965922c5abae8cb809
Vladimir Khusainov <vlad@emcraft.com>
2013-12-19 13:25:49 +0400

    RT92026. U-boot port for STM32F429 Discovery. Cosmetic change to the U-Boot banner print-out.

--- include/configs/stm32f429-discovery.h
+++ include/configs/stm32f429-discovery.h
@@ -58,7 +58,7 @@
 #define CONFIG_DISPLAY_CPUINFO		1
 #define CONFIG_DISPLAY_BOARDINFO	1
 
-#define CONFIG_SYS_BOARD_REV_STR	"Rev 1.A"
+#define CONFIG_SYS_BOARD_REV_STR	"1.A"
 
 /*
  * Monitor prompt
