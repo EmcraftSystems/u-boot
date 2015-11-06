85726537dd132cf4c9e22ef75ffd011a002a52a7
Yuri Tikhonov <yur@emcraft.com>
2015-11-06 10:55:55 +0300

    RT #113629. Fix-up ethernet address in DTB
    Signed-off-by: Yuri Tikhonov <yur@emcraft.com>

--- lib_arm/bootm.c
+++ lib_arm/bootm.c
@@ -231,6 +231,7 @@ static int bootm_linux_fdt(int machid, bootm_headers_t *images)
 	fdt_chosen(*of_flat_tree, 1);
 
 	fixup_memory_node(*of_flat_tree);
+	fdt_fixup_ethernet(*of_flat_tree);
 
 	fdt_initrd(*of_flat_tree, *initrd_start, *initrd_end, 1);
 
